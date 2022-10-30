64877d551f7dfd1754a501776267b199caaa3834
JCaloca <jacobcaloca1@gmail.com>
2022-10-30 11:23:23 -0700

    Created seeds sql to fill in schema

--- db/schema.sql
+++ db/schema.sql
@@ -18,10 +18,11 @@ CREATE TABLE roles (
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    ON DELETE SET NULL
 );
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO departments (id, department_name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Finance"),
+       ("Legal");
+       
+
+-- May need to add department_id
+INSERT INTO roles (title, salary, department_id) 
+VALUES ("Sales Lead", 120000, 1),
+       ("Salesperson", 95000, 1),
+       ("Lead Engineer", 210000, 2),
+       ("Software Engineer", 180000, 2),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 4),
+       ("Lawyer", 190000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, NULL),
+       ("Stacy", "Adams", 2, 1),
+       ("Tony", "Stark", 3, NULL),
+       ("Scott", "Lang", 4, 3),
+       ("Wade", "Wilson", 5, NULL),
+       ("Natasha", "Romanoff", 6, 5),
+       ("Jennifer", "Walters", 7, NULL),
+       ("Thor", "Odinson", 8, 7);
+       
