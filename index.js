f1369462d6b0722d66a8c21a4fe87ebb8d61ffc1
JCaloca <jacobcaloca1@gmail.com>
2022-10-31 08:28:53 -0700

    Created if else statements for users selection in initial prompt, and wrapped the prompt in a function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const cTable = require("console.table");
+const { exit } = require("process");
 
 // Creating connection to database
 const connection = mysql.createConnection({
@@ -11,22 +12,46 @@ const connection = mysql.createConnection({
 });
 
 // TODO: Add inquirer questions for first prompt
-inquirer.prompt([
-  {
-    type: "list",
-    name: "choice",
-    message: "What would you like to do?",
-    choices: [
-      "View all departments.",
-      "View all roles.",
-      "View all employees.",
-      "Add a department.",
-      "Add a role.",
-      "Add an employee.",
-      "Update an employee role.",
-    ],
-  },
-]);
+const initialPrompt = () => {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "userChoice",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+          "Exit",
+        ],
+      },
+    ])
+    .then((answers) => {
+      const { userChoice } = answers;
+      if (userChoice === "View all departments") {
+        viewDepartments();
+      } else if (userChoice === "View all roles") {
+        viewRoles();
+      } else if (userChoice === "View all employees") {
+        viewEmployees();
+      } else if (userChoice === "Add a department") {
+        addDepartment();
+      } else if (userChoice === "Add a role") {
+        addRole();
+      } else if (userChoice === "Add an employee") {
+        addEmployee();
+      } else if (userChoice === "Update an employee role") {
+        updateEmployee();
+      } else if (userChoice === "Exit") {
+        exit();
+      }
+    });
+};
 
 // TODO: add function/ prompt for view all departments
 
@@ -41,3 +66,5 @@ inquirer.prompt([
 // TODO: add function/ prompt for add an employee
 
 // TODO: add function/ prompt for updating an employee
+
+initialPrompt();
